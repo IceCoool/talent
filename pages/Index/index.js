e98ef09d6fd027b46c58ba2b1418ae619a00242a
IceCoool <461258697@qq.com>
2020-02-27 22:37:25 +0800

    index

--- pages/Index/index.js
+++ pages/Index/index.js
@@ -5,62 +5,89 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    staffFilterTop: Number,
+    showFxq: false,
+    showMyXq: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady: function() {
+    wx.createSelectorQuery().select('#staff-filter').boundingClientRect().exec((res) => {
+      this.setData({
+        staffFilterTop: res[0].top
+      })
+    })
   },
+  onPageScroll: function(e) {
+    const staffFilterTop = this.data.staffFilterTop
+    if (e.scrollTop >= staffFilterTop - 68) {
+      this.setData({
+        showFxq: true
+      })
+    } else {
+      this.setData({
+        showFxq: false
+      })
+    }
 
+    // if (e.scrollTop >= staffFilterTop - 104) {
+    //   this.setData({
+    //     showMyXq: true
+    //   })
+    // } else {
+    //   this.setData({
+    //     showMyXq: false
+    //   })
+    // }
+  },
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
